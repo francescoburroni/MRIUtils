f06d4844a2a121adbccc6416bca44ed0c7643428
Francesco Burroni <burronif@icloud.com>
2026-03-24 08:20:07 +0100

    Update extractROIProperties.m

--- extractROIProperties.m
+++ extractROIProperties.m
@@ -5,6 +5,7 @@ arguments (Input)
     mask
     opts.flipud logical = false;
     opts.rescaleFactor double = 1;
+    opts.doPlot logical = false
 end
 
 arguments (Output)
@@ -12,17 +13,17 @@ arguments (Output)
     CentroidROIPosition
 end
 
-  for k = 1 : numel(S)    
-      if opts.flipud          
+for k = 1 : numel(S)
+    if opts.flipud
         mri = imresize(flipud(S(k).mri),opts.rescaleFactor);
-      else
-         mri = imresize(S(k).mri,opts.rescaleFactor);
-      end
-      
+    else
+        mri = imresize(S(k).mri,opts.rescaleFactor);
+    end
+
     meanInt = nan(size(mri,3),1);
     centroidInt = nan(size(mri,3),2);
-    
-    shg 
+
+    shg
     for l = 1 : size(mri,3)
         frame = mri(:,:,l);
         frameROI = frame(mask.array);
@@ -31,20 +32,55 @@ end
         % get centroid
         maskedFrame = double(frame) .* (mask.array);
         props = regionprops(mask.array, maskedFrame, "WeightedCentroid");
+        if numel(props) == 1
+            centroidInt(l,1:2) = props.WeightedCentroid;
+        else
+            centroidInt(l,1:2) = selectCentroid(props, mask.name);
+            warning('extractROIProperties:multipleRegions', ...
+                'Mask "%s" has %d connected components at frame %d. Selecting %s component.', ...
+                mask.name, numel(props), l, selectionRule(mask.name));
+        end
         centroidInt(l,1:2) = props.WeightedCentroid; % [x, y]
-
+        
+        if opts.doPlot
         nexttile(1)
         imagesc(frame); hold on;
-        plot(centroidInt(l,1),centroidInt(l,2),"wo"); hold on;
-        xlim([40 120])
-        ylim([40 120])
+        colormap("bone")
+        plot(centroidInt(l,1),centroidInt(l,2),"w.",MarkerSize=25); hold on;
         pause(.25)
-        shg
+        end
 
     end
     frameROIMeanInt{k} =    meanInt;
     CentroidROIPosition{k} = centroidInt;
 
-  end
+end
 
+end
+
+function rule = selectionRule(maskName)
+switch lower(maskName)
+    case {'larynx','velum'}
+        rule = 'rightmost';
+    case 'lips'
+        rule = 'largest';
+    otherwise
+        rule = 'largest';
+end
+end
+
+function centroid = selectCentroid(props, maskName)
+centroids = vertcat(props.WeightedCentroid);  % x, y per component
+areas     = vertcat(props.Area);
+
+switch lower(maskName)
+    case {'larynx', 'velum'}
+        % rightmost centroid (largest x)
+        [~, idx] = max(centroids(:,1));
+        centroid = centroids(idx,:);
+    case 'lips'
+        % largest component or return both for aperture computation
+        [~, idx] = max(areas);
+        centroid = centroids(idx,:);
+end
 end
